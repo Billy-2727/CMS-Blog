448a34c4ffc08a3509fa2bc3a74916743fad510b
Billy-2727 <81046105+Billy-2727@users.noreply.github.com>
2021-04-02 20:20:16 +0100

    v3

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+
 import Layout from "../components/layout"
 import Head from "../components/head"
 
@@ -9,8 +9,21 @@ const AboutPage = () => {
       <Head title="About" />
       <div>
         <h1>About</h1>
-        <p>This is my about AboutPage</p>
-        <p>Need a Dev?</p> <Link to="/contact">Contact Me</Link>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tellus
+          ligula, volutpat nec pretium sit amet, dictum vitae augue. Praesent
+          suscipit tincidunt lacus, non egestas est molestie eu. Pellentesque id
+          dictum magna, vitae iaculis dolor. Nulla eu augue eget lorem hendrerit
+          laoreet sit amet quis eros. Etiam sed iaculis metus. Sed ex quam,
+          eleifend et est vel, rutrum laoreet lorem. Aenean ultricies, orci eget
+          vulputate dictum, mi erat porttitor sapien, a suscipit eros neque non
+          est. Orci varius natoque penatibus et magnis dis parturient montes,
+          nascetur ridiculus mus. In tincidunt tortor sed nibh vulputate
+          ullamcorper. Cras nec ultrices libero. Praesent iaculis tellus est.
+          Sed gravida leo purus, eu ultrices sem pulvinar in. Donec nec velit
+          turpis. Cras eget cursus ipsum. In ornare nunc ut urna scelerisque
+          bibendum. Duis vitae bibendum arcu, elementum dapibus leo.
+        </p>{" "}
       </div>
     </Layout>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Head from "../components/head"
 
@@ -7,9 +6,22 @@ const IndexPage = () => {
   return (
     <Layout>
       <Head title="Home" />
-      <h1>CMS Blog</h1>
-      <h2>This is a cms blog</h2>
-      <p>Need a Dev?</p> <Link to="/contact">Contact Me</Link>
+      <h1>CMS Blog Breakdown</h1>
+      <p>
+        This blog is an example of what can be done using gatsby and a cms such
+        as Contentful which is used here.
+      </p>
+      <p>
+        The basic premise of this is that anyone can update and add blogs to the
+        blog section without any developer interaction using the combination
+        <strong> Contentful</strong> ,<strong> Gatsby</strong> and
+        <strong> Netlify</strong>.
+      </p>
+      <p>
+        This website also uses <strong>GraphQL</strong> to query different parts
+        of the Contentful source data to then dynamically display all the
+        necessary data found in the blog section.
+      </p>
     </Layout>
   )
 }
